3a1e298c64919864459140a2879788c9c186a345
Parker Moore <parkrmoore@gmail.com>
2011-02-10 15:00:58 -0600

    added a form functionality

--- index.php
+++ index.php
@@ -1,30 +1,83 @@
+<?php
+if($_POST){
+	include_once("shortener.php");
+	include_once("include.php");
+	$googer = new GoogleURLAPI($key);
+	if($_POST['action'] == "shorten"){
+		$shortened = $googer->shorten($_POST['to_shorten']);
+		$expanded = $_POST['to_shorten'];
+		$short = 1;
+	}elseif($_POST['action'] == "expand"){
+		$expanded = $googer->expand($_POST['to_expand']);
+		$shortened = $_POST['to_expand'];
+		$short = 2;
+	}
+}else{
+	$expanded = "http://";
+	$shortened = "http://goo.gl/";
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
    "http://www.w3.org/TR/html4/strict.dtd">
 
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>untitled</title>
+	<title>Shortener/Expander: Google URL API</title>
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Parker Moore">
 	<!-- Date: 2011-02-10 -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+	<script type="text/javascript">
+		JQuery.fn.center = function () {
+					    this.css("position","absolute");
+					    this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
+					    this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+					    return this;
+					}
+		$(document).ready(function(){
+			$("#container").center();
+		});
+	</script>
+	<style type="text/css">
+	body, html {
+		margin: 0;
+		padding: 0;
+		font-size: 20px;
+		font-family: "Geneva";
+		text-align: center;
+	}
+	#container {
+		width: 600px;
+		margin: 0 auto;
+	}
+	input {
+		font-size: 40px;
+		text-align: center;
+	}
+	</style>
 </head>
 <body>
-<?php
-include_once("shortener.php");
-// Create instance with key
-$key = 'AIzaSyBCykB_Wlz94Ek7AHkXvfDybyNfgDAaypw';
-$googer = new GoogleURLAPI($key);
-
-// Test: Shorten a URL
-$shortDWName = $googer->shorten("http://davidwalsh.name");
-echo $shortDWName; // returns http://goo.gl/i002
-
-// Test: Expand a URL
-$longDWName = $googer->expand($shortDWName);
-echo $longDWName; // returns http://davidwalsh.name
-
-
-?>
+	<div id="container">
+	<form id="shorten" method="post">
+		<input type="hidden" value="shorten" name="action">
+		<input type="text" value="<?php echo $expanded; ?>" name="to_shorten">
+		<input type="Submit" value="Shorten">
+	</form><br>
+	<form id="expand" method="post">
+		<input type="hidden" value="expand" name="action">
+		<input type="text" value="<?php echo $shortened; ?>" name="to_expand">
+		<input type="Submit" value="Expand">
+	</form>
+	<?php
+	if($short==1){
+		echo "Result:<br>";
+		echo $shortened;
+	}elseif($short==2){
+		echo "Result:<br>";
+		echo $expanded;
+	}
+	?>
+	</div>
 </body>
 </html>
